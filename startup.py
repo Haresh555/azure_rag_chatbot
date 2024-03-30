a39fb3ceb3d9ac68270af69483d047fc784b9010
Hareshkumar.K.M <mailme.hareshkumar@gmail.com>
2024-03-30 15:18:05 +0530

    Update startup.py

--- startup.py
+++ startup.py
@@ -1,11 +1,15 @@
+import os
 import subprocess
 import shutil
+import os
+if not os.path.exists('.chainlit'):
+    os.makedirs('.chainlit')
 
-with open('.chainlit/config.toml' , 'r') as cl_f:
+with open('config.toml' , 'r') as cl_f:
     toml_content = cl_f.read()
     cl_f.close()
 
-with open('config.toml', 'r') as f:
+with open('.chainlit/config.toml', 'w') as f:
     f.write(toml_content)
     f.close()
 print(toml_content)
